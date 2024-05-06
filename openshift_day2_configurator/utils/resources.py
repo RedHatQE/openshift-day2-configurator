fd9bae817e0e916dfedd5943f3a9144387dfcb62
rnetser <rnetser@redhat.com>
2024-05-06 12:15:57 +0300

    update tests

--- openshift_day2_configurator/utils/resources.py
+++ openshift_day2_configurator/utils/resources.py
@@ -4,15 +4,17 @@ from typing import Any, Dict
 
 def create_ocp_resource(ocp_resource: Any, logger: logging.Logger) -> Dict[str, Any]:
     name = ocp_resource.name
+    resource_kind = ocp_resource.kind
 
     try:
         if ocp_resource.exists:
-            return {"res": False, "err": f"ClusterRole {name} already exists"}
+            return {"res": False, "err": f"{resource_kind} {name} already exists"}
         else:
             logger.debug(f"Create LDAP groups sync cluster role {name}")
             ocp_resource.deploy()
             return {"res": True, "err": None}
 
     except Exception as ex:
-        logger.debug(f"Failed to create {ocp_resource.kind} {name} with error {ex}")
-        return {"res": False, "err": ex.__repr__()}
+        _ex: str = str(ex) if isinstance(ex, Exception) else ex.__repr__()
+        logger.debug(f"Failed to create {resource_kind} {name} with error {_ex}")
+        return {"res": False, "err": _ex}
