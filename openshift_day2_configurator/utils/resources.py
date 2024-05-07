be7f914fe0845b948138581a3460b8bcb58df78a
Ruth Netser <rnetser@redhat.com>
2024-05-07 10:08:11 +0300

    Add more LDAP resources (#21)
    * initial code
    
    * initial code
    
    * initial code
    
    * initial code
    
    * add ldap configs
    
    * add ldap configs
    
    * add ldap configs
    
    * add ldap configs
    
    * in-solidarity
    
    * update tests
    
    * update sealedsecret
    
    * update param name and remove redundant one

---
+++ openshift_day2_configurator/utils/resources.py
@@ -0,0 +1,20 @@
+import logging
+from typing import Any, Dict
+
+
+def create_ocp_resource(ocp_resource: Any, logger: logging.Logger) -> Dict[str, Any]:
+    name = ocp_resource.name
+    resource_kind = ocp_resource.kind
+
+    try:
+        if ocp_resource.exists:
+            return {"res": False, "err": f"{resource_kind} {name} already exists"}
+        else:
+            logger.debug(f"Create LDAP groups sync cluster role {name}")
+            ocp_resource.deploy()
+            return {"res": True, "err": None}
+
+    except Exception as ex:
+        _ex: str = str(ex) if isinstance(ex, Exception) else ex.__repr__()
+        logger.debug(f"Failed to create {resource_kind} {name} with error {_ex}")
+        return {"res": False, "err": _ex}
